08202edaadb31adf9f769fa4dbcf5c5e92758e8f
hamzazeamari <zeam.hamza@gmail.com>
2022-06-09 23:42:53 +0200

    End

--- Apprentissage/channels/main.go
+++ Apprentissage/channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -14,16 +15,29 @@ func main() {
 		"http://twitter.com",
 	}
 
+	channel := make(chan string)
+
 	for _, site := range urlSites {
-		checkLink(site)
+		go checkLink(site, channel)
 	}
+
+	for l := range channel {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, channel)
+		}(l)
+	}
+
 }
 
-func checkLink(link string) {
+func checkLink(link string, channel chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, " est OUT")
+		channel <- link
 	} else {
 		fmt.Println(link, " est IN")
+		channel <- link
+
 	}
 }
