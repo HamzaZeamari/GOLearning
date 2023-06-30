6a37e7d48deba64b045587e8207dc5ed315804d0
Zeamari Hamza <zeam.hamza@gmail.com>
2023-06-30 19:39:48 +0200

    POST finished

--- challenge/test2/main.go
+++ challenge/test2/main.go
@@ -1,5 +1,49 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"io"
+	"net/http"
+	"os"
+)
 
+func createAWord(c *gin.Context) {
+
+	word := c.Param("word")
+
+	// Ouvre le fichier JSON
+	file, err := os.OpenFile("words.json", os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to open file"})
+		return
+	}
+	defer file.Close()
+
+	// Décode le contenu JSON dans une variable
+	var words []string
+	err = json.NewDecoder(file).Decode(&words)
+	if err != nil && err != io.EOF {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to decode file"})
+		return
+	}
+
+	// Ajoute le nouveau mot à la variable
+	words = append(words, word)
+
+	// Réécrit le contenu JSON dans le fichier
+	file.Seek(0, 0)
+	err = json.NewEncoder(file).Encode(words)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to encode file"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Word added successfully"})
+}
+
+func main() {
+	router := gin.Default()
+	router.POST("/service/word=:word", createAWord)
+	router.Run("localhost:8080")
 }
