72773d1d6e32072053a459b338f3adaf87b8b7aa
Zeamari Hamza <zeam.hamza@gmail.com>
2023-07-02 11:22:46 +0200

    Update challenge

--- challenge/test2/main.go
+++ challenge/test2/main.go
@@ -5,19 +5,16 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 func getMostFrequentWord(c *gin.Context) {
+	// GET the prefix from request
+	prefix := c.Param("prefix")
 
-}
-
-func createAWord(c *gin.Context) {
-
-	word := c.Param("word")
-
-	// Ouvre le fichier JSON
+	// Open JSON file
 	file, err := os.OpenFile("words.json", os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to open file"})
@@ -25,7 +22,7 @@ func createAWord(c *gin.Context) {
 	}
 	defer file.Close()
 
-	// Décode le contenu JSON dans une variable
+	// Decode JSON content into a variable
 	var words []string
 	err = json.NewDecoder(file).Decode(&words)
 	if err != nil && err != io.EOF {
@@ -33,10 +30,51 @@ func createAWord(c *gin.Context) {
 		return
 	}
 
-	// Ajoute le nouveau mot à la variable
+	// Count for every word containing the prefix in a map
+	counts := make(map[string]int)
+	for _, word := range words {
+		if strings.HasPrefix(word, prefix) {
+			counts[word]++
+		}
+	}
+
+	// Check which word is the most frequent using counting map
+	maxCount := 0
+	mostFrequentWord := "null"
+	for word, count := range counts {
+		if count > maxCount {
+			maxCount = count
+			mostFrequentWord = word
+		}
+	}
+	//Return the Status : OK and add the most frequent word and the number of occurence
+	c.JSON(http.StatusOK, gin.H{"word": mostFrequentWord, "count": maxCount})
+}
+
+func createAWord(c *gin.Context) {
+
+	word := c.Param("word")
+
+	// Open JSON file
+	file, err := os.OpenFile("words.json", os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to open file"})
+		return
+	}
+	defer file.Close()
+
+	// Decode JSON content into a variable
+	var words []string
+	err = json.NewDecoder(file).Decode(&words)
+	if err != nil && err != io.EOF {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to decode file"})
+		return
+	}
+
+	// Add new word
 	words = append(words, word)
 
-	// Réécrit le contenu JSON dans le fichier
+	// Write JSON content in words.json
 	file.Seek(0, 0)
 	err = json.NewEncoder(file).Encode(words)
 	if err != nil {
@@ -44,12 +82,15 @@ func createAWord(c *gin.Context) {
 		return
 	}
 
+	// Return the Status : OK
 	c.JSON(http.StatusOK, gin.H{"message": "Word added successfully"})
 }
 
 func main() {
 	router := gin.Default()
+	// Route POST : /service/word=:word
 	router.POST("/service/word=:word", createAWord)
+	// Route GET : /service/prefix=:prefix
 	router.GET("/service/prefix=:prefix", getMostFrequentWord)
 	router.Run("localhost:8080")
 }
